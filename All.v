3660736eae12dbc8d0bbb9cbe81b089f167d0757
Guillaume Claret <dev@clarus.me>
2015-02-19 17:06:34 +0100

    Unix test module removed

--- All.v
+++ All.v
@@ -57,36 +57,3 @@ Module Run.
     t c_x x -> t (c_f x) y -> t (C.Let c_x c_f) y
   | Intro : forall {A} (B : Type) {c : C.t E A} {x : A}, (B -> t c x) -> t c x.
 End Run.
-
-Module Unix.
-  Require Import ListString.All.
-
-  Inductive t :=
-  (** List the files of a directory. *)
-  | ListFiles (directory : LString.t)
-  (** Read the content of a file. *)
-  | ReadFile (file_name : LString.t)
-  (** Update (or create) a file with some content. *)
-  | WriteFile (file_name : LString.t) (content : LString.t)
-  (** Delete a file. *)
-  | DeleteFile (file_name : LString.t)
-  (** Run a command. *)
-  | System (command : LString.t)
-  (** Print a message on the standard output. *)
-  | Print (message : LString.t).
-
-  (** The type of an answer for a command depends on the value of the command. *)
-  Definition answer (command : t) : Type :=
-    match command with
-    | ListFiles _ => option (list LString.t)
-    | ReadFile _ => option LString.t
-    | WriteFile _ _ => bool
-    | DeleteFile _ => bool
-    | System _ => option bool
-    | Print _ => bool
-    end.
-
-  Definition effect : Effects.t := {|
-    Effects.command := t;
-    Effects.answer := answer |}.
-End Unix.

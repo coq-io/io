c1e92a78b45a4b46aa4b8090b0f8483d482477a2
Guillaume Claret <dev@clarus.me>
2015-03-11 11:59:23 +0100

    Run of concurrent computations

--- src/Run.v
+++ src/Run.v
@@ -11,4 +11,10 @@ Inductive t : forall {E : Effects.t} {A : Type}, C.t E A -> A -> Type :=
   t (C.Call E command) answer
 | Let : forall {E A B} {c_x : C.t E B} {x : B} {c_f : B -> C.t E A} {y : A},
   t c_x x -> t (c_f x) y -> t (C.Let c_x c_f) y
+| Join : forall {E A B} {c_x : C.t E A} {x : A} {c_y : C.t E B} {y : B},
+  t c_x x -> t c_y y -> t (C.Join c_x c_y) (x, y)
+| Left : forall {E A B} {c_x : C.t E A} {x : A} {c_y : C.t E B},
+  t c_x x -> t (C.First c_x c_y) (inl x)
+| Right : forall {E A B} {c_x : C.t E A} {c_y : C.t E B} {y : B},
+  t c_y y -> t (C.First c_x c_y) (inr y)
 | Intro : forall {E A} (B : Type) {c : C.t E A} {x : A}, (B -> t c x) -> t c x.

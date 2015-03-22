4f0d4fdf58edaae38329f3b3abc82f50eb7af440
Guillaume Claret <dev@clarus.me>
2015-03-22 21:42:14 +0100

    Generic run

--- src/C.v
+++ src/C.v
@@ -13,6 +13,18 @@ Arguments Let {E A B} _ _.
 Arguments Join {E A B} _ _.
 Arguments First {E A B} _ _.
 
+(** A run from an effect to a more general effect. *)
+Fixpoint run {E1 E2 : Effect.t} {A : Type}
+  (run_command : forall (c : Effect.command E1), C.t E2 (Effect.answer E1 c))
+  (x : C.t E1 A) : C.t E2 A :=
+  match x with
+  | C.Ret _ x => C.Ret x
+  | C.Call c => run_command c
+  | C.Let _ _ x f => C.Let (run run_command x) (fun x => run run_command (f x))
+  | C.Join _ _ x y => C.Join (run run_command x) (run run_command y)
+  | C.First _ _ x y => C.First (run run_command x) (run run_command y)
+  end.
+
 (** Some optional notations. *)
 Module Notations.
   (** A nicer notation for `Ret`. *)

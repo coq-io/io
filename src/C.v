858db8e65006ba0425eebac2e68f3c2793f794e2
Guillaume Claret <dev@clarus.me>
2015-03-24 15:45:16 +0100

    Computation constructors with implicit arguments to be matched with both Coq 8.4 and 8.5

--- src/C.v
+++ src/C.v
@@ -4,61 +4,59 @@ Require Import Effect.
 Inductive t (E : Effect.t) : Type -> Type :=
 | Ret : forall {A : Type} (x : A), t E A
 | Call : forall (command : Effect.command E), t E (Effect.answer E command)
-| Let : forall {A B : Type}, t E A -> (A -> t E B) -> t E B
+| Let : forall (A B : Type), t E A -> (A -> t E B) -> t E B
 | Join : forall {A B : Type}, t E A -> t E B -> t E (A * B)
 | First : forall {A B : Type}, t E A -> t E B -> t E (A + B).
-Arguments Ret {E A} _.
-Arguments Call E _.
-Arguments Let {E A B} _ _.
-Arguments Join {E A B} _ _.
-Arguments First {E A B} _ _.
+Arguments Call {E} _.
+Arguments Let {E} _ _ _ _.
+Arguments Join {E} _ _ _ _.
+Arguments First {E} _ _ _ _.
 
 (** A nicer notation for `Ret`. *)
 Definition ret {E : Effect.t} {A : Type} (x : A) : t E A :=
-  Ret x.
+  Ret E x.
 
 (** A nicer notation for `Call`. *)
 Definition call (E : Effect.t) (command : Effect.command E)
   : t E (Effect.answer E command) :=
-  Call E command.
+  Call (E := E) command.
 
 (** A nicer notation for `Join`. *)
 Definition join {E : Effect.t} {A B : Type} (x : t E A) (y : t E B)
   : t E (A * B) :=
-  Join x y.
+  Join _ _ x y.
 
 (** A nicer notation for `First`. *)
 Definition first {E : Effect.t} {A B : Type} (x : t E A) (y : t E B)
   : t E (A + B) :=
-  First x y.
+  First _ _ x y.
 
 (** A run from an effect to a more general effect. *)
 Fixpoint run {E1 E2 : Effect.t} {A : Type}
   (run_command : forall (c : Effect.command E1), C.t E2 (Effect.answer E1 c))
-  (x : C.t E1 A) : C.t E2 A.
-  destruct x as [A x | c | A B x f | A B x y | A B x y].
-  - exact (C.Ret x).
-  - exact (run_command c).
-  - exact (C.Let (run _ _ _ run_command x) (fun x =>
-      run _ _ _ run_command (f x))).
-  - exact (C.Join (run _ _ _ run_command x) (run _ _ _ run_command y)).
-  - exact (C.First (run _ _ _ run_command x) (run _ _ _ run_command y)).
-Defined.
+  (x : C.t E1 A) : C.t E2 A :=
+  match x with
+  | Ret _ x => Ret _ x
+  | Call c => run_command c
+  | Let _ _ x f => Let _ _ (run run_command x) (fun x => run run_command (f x))
+  | Join _ _ x y => Join _ _ (run run_command x) (run run_command y)
+  | First _ _ x y => First _ _ (run run_command x) (run run_command y)
+  end.
 
 (** Some optional notations. *)
 Module Notations.
   (** A nicer notation for `Let`. *)
   Notation "'let!' x ':=' X 'in' Y" :=
-    (Let X (fun x => Y))
+    (Let _ _ X (fun x => Y))
     (at level 200, x ident, X at level 100, Y at level 200).
 
   (** Let with a typed answer. *)
   Notation "'let!' x : A ':=' X 'in' Y" :=
-    (Let X (fun (x : A) => Y))
+    (Let _ _ X (fun (x : A) => Y))
     (at level 200, x ident, X at level 100, A at level 200, Y at level 200).
 
   (** Let ignoring the unit answer. *)
   Notation "'do!' X 'in' Y" :=
-    (Let X (fun (_ : unit) => Y))
+    (Let _ _ X (fun (_ : unit) => Y))
     (at level 200, X at level 100, Y at level 200).
 End Notations.
--- src/Spec.v
+++ src/Spec.v
@@ -4,14 +4,14 @@ Require Import C.
 (** A specification is an execution of a computation with explicit answers for
     the external calls. *)
 Inductive t : forall {E : Effect.t} {A : Type}, C.t E A -> A -> Type :=
-| Ret : forall {E A} (x : A), t (C.Ret (E := E) x) x
+| Ret : forall {E A} (x : A), t (C.Ret E x) x
 | Call : forall E (c : Effect.command E) (answer : Effect.answer E c),
-  t (C.Call E c) answer
-| Let : forall {E A B} {c_x : C.t E B} {x : B} {c_f : B -> C.t E A} {y : A},
-  t c_x x -> t (c_f x) y -> t (C.Let c_x c_f) y
+  t (C.Call (E := E) c) answer
+| Let : forall {E A B} {c_x : C.t E A} {x : A} {c_f : A -> C.t E B} {y : B},
+  t c_x x -> t (c_f x) y -> t (C.Let A B c_x c_f) y
 | Join : forall {E A B} {c_x : C.t E A} {x : A} {c_y : C.t E B} {y : B},
-  t c_x x -> t c_y y -> t (C.Join c_x c_y) (x, y)
+  t c_x x -> t c_y y -> t (C.Join A B c_x c_y) (x, y)
 | Left : forall {E A B} {c_x : C.t E A} {x : A} {c_y : C.t E B},
-  t c_x x -> t (C.First c_x c_y) (inl x)
+  t c_x x -> t (C.First A B c_x c_y) (inl x)
 | Right : forall {E A B} {c_x : C.t E A} {c_y : C.t E B} {y : B},
-  t c_y y -> t (C.First c_x c_y) (inr y).
+  t c_y y -> t (C.First A B c_x c_y) (inr y).

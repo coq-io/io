7fa0b3f36c9f53ca0a08ca01f4a96c873d6e2b51
Guillaume Claret <dev@clarus.me>
2015-05-10 18:29:53 +0200

    Smart constructors removed

--- src/C.v
+++ src/C.v
@@ -25,44 +25,29 @@ Definition call (E : Effect.t) (command : Effect.command E)
   : t E (Effect.answer E command) :=
   Call (E := E) command.
 
-(** A smarter `Let`. *)
-Definition smart_let {E : Effect.t} {A B : Type} (x : t E A)
-  : (A -> t E B) -> t E B :=
-  match x with
-  | Ret _ v_x => fun f => f v_x
-  | x => fun f => Let _ _ x f
-  end.
-
 (** A nicer notation for `Choose`. *)
 Definition choose {E : Effect.t} {A : Type} (x1 x2 : t E A) : t E A :=
   Choose A x1 x2.
 
-(** A smarter `Join`. *)
+(** A nicer notation for `Join`. *)
 Definition join {E : Effect.t} {A B : Type} (x : t E A) (y : t E B)
   : t E (A * B) :=
-  match x with
-  | Ret _ v_x => Let _ _ y (fun v_y => Ret _ (v_x, v_y))
-  | x =>
-    match y with
-    | Ret _ v_y => Let _ _ x (fun v_x => Ret _ (v_x, v_y))
-    | y => Join _ _ x y
-    end
-  end.
+  Join A B x y.
 
 (** Some optional notations. *)
 Module Notations.
   (** A nicer notation for `Let`. *)
   Notation "'let!' x ':=' X 'in' Y" :=
-    (smart_let X (fun x => Y))
+    (Let _ _ X (fun x => Y))
     (at level 200, x ident, X at level 100, Y at level 200).
 
   (** Let with a typed answer. *)
   Notation "'let!' x : A ':=' X 'in' Y" :=
-    (smart_let X (fun (x : A) => Y))
+    (Let _ _ X (fun (x : A) => Y))
     (at level 200, x ident, X at level 100, A at level 200, Y at level 200).
 
   (** Let ignoring the unit answer. *)
   Notation "'do!' X 'in' Y" :=
-    (smart_let X (fun (_ : unit) => Y))
+    (Let _ _ X (fun (_ : unit) => Y))
     (at level 200, X at level 100, Y at level 200).
 End Notations.
